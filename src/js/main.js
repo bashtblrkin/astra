7c757b5425e3cf6defc8a28a3941131f376183df
bashtblrkin <vanya.baturin@list.ru>
2022-02-15 23:40:41 +0300

    upd

--- src/js/main.js
+++ src/js/main.js
@@ -64,7 +64,19 @@ function activateSlick() {
         slidesToShow: 1,
         slidesToScroll: 1,
         arrows: false,
-        dots: true
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 5000
+    })
+
+    $('.slider-specialists').slick({
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        arrows: false,
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 5000
     })
 }
 
